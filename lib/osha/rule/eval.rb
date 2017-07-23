8112d8fa307c9977c57895697847a8f0f43ee566
Blake Williams <blake@blakewilliams.me>
2017-07-23 19:59:51 -0400

    Add instance_exec to eval rule

--- lib/osha/rule/eval.rb
+++ lib/osha/rule/eval.rb
@@ -1,8 +1,15 @@
 module OSHA::Rule
   class Eval < Rule
     def run(sexp)
-      if sexp.sexp_type == :call && sexp[2] == :eval
+      return unless sexp.sexp_type == :call
+
+      case sexp[2]
+      when :eval
         create_warning("Use of eval", sexp)
+      when :instance_eval
+        create_warning("Use of instance_eval", sexp)
+      else
+        nil
       end
     end
   end
